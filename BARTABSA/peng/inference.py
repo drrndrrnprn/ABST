8d3c42189029de04679aa228553316b812d315f0
drrndrrnprn <38527355+drrndrrnprn@users.noreply.github.com>
2021-08-24 00:28:16 +0900

    comment out

--- BARTABSA/peng/inference.py
+++ BARTABSA/peng/inference.py
@@ -11,39 +11,40 @@ import torch
 
 
 from data.pipe import BartBPEABSAPipe
-from peng.model.bart_absa import BartSeq2SeqModel
-from peng.model.generator import SequenceGeneratorModel
+#from peng.model.bart_absa import BartSeq2SeqModel
+#from peng.model.generator import SequenceGeneratorModel
 from peng.model.predictor import Predictor
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
 parser.add_argument('--dataset_name', default='pengb/16res', type=str)
-parser.add_argument('--num_beams', default=4, type=int)
+#parser.add_argument('--num_beams', default=4, type=int)
 parser.add_argument('--opinion_first', action='store_true', default=False)
-parser.add_argument('--decoder_type', default='avg_score', type=str, choices=['None', 'avg_score'])
-parser.add_argument('--length_penalty', default=1.0, type=float)
+#parser.add_argument('--decoder_type', default='avg_score', type=str, choices=['None', 'avg_score'])
+#parser.add_argument('--length_penalty', default=1.0, type=float)
 parser.add_argument('--bart_name', default='facebook/bart-base', type=str)
-parser.add_argument('--use_encoder_mlp', type=int, default=1)
+#parser.add_argument('--use_encoder_mlp', type=int, default=1)
 
 args= parser.parse_args()
 
 model_path = args.model_path
-num_beams = args.num_beams
 dataset_name = args.dataset_name
+
+#num_beams = args.num_beams
 opinion_first = args.opinion_first
-length_penalty = args.length_penalty
+#length_penalty = args.length_penalty
 if isinstance(args.decoder_type, str) and args.decoder_type.lower() == 'none':
-    args.decoder_type = None
-decoder_type = args.decoder_type
+    #args.decoder_type = None
+#decoder_type = args.decoder_type
 bart_name = args.bart_name
-use_encoder_mlp = args.use_encoder_mlp
+#use_encoder_mlp = args.use_encoder_mlp
 
 def get_data():
     pipe = BartBPEABSAPipe(tokenizer=bart_name, opinion_first=opinion_first)
     data_bundle = pipe.process_from_file(f'../data/{dataset_name}')
     return data_bundle, pipe.tokenizer, pipe.mapping2id
-
+'''
 data_bundle, tokenizer, mapping2id = get_data()
 max_len = 10
 max_len_a = {
@@ -68,7 +69,7 @@ model = SequenceGeneratorModel(model, bos_token_id=bos_token_id,
                                max_length=max_len, max_len_a=max_len_a,num_beams=num_beams, do_sample=False,
                                repetition_penalty=1, length_penalty=length_penalty, pad_token_id=eos_token_id,
                                restricter=None)
-
+'''
 device = torch.device("cuda")
 #model.load_state_dict(torch.load(model_path))
 model = torch.load(model_path)
