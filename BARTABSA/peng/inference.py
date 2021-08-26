248e4a85f7b0628b29c7f18eda8fc5f7de7bb1e0
drrndrrnprn <38527355+drrndrrnprn@users.noreply.github.com>
2021-08-27 01:10:12 +0900

    json output

--- BARTABSA/peng/inference.py
+++ BARTABSA/peng/inference.py
@@ -3,7 +3,6 @@ sys.path.append('../')
 import os
 if 'p' in os.environ:
     os.environ['CUDA_VISIBLE_DEVICES'] = os.environ['p']
-import pprint
 import itertools
 
 import numpy as np
@@ -11,13 +10,15 @@ import torch
 
 from data.pipe import BartBPEABSAPipe
 from peng.model.predictor import Predictor
+from peng.model.utils import process_aos, output_json
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
+parser.add_argument('--bart_name', default='facebook/bart-base', type=str)
 parser.add_argument('--dataset_name', default='pengb/16res', type=str)
 parser.add_argument('--opinion_first', action='store_true', default=False)
-parser.add_argument('--bart_name', default='facebook/bart-base', type=str)
+parser.add_argument('--use_output_polarity', action='store_true', default=False)
 
 args= parser.parse_args()
 
@@ -26,6 +27,7 @@ dataset_name = args.dataset_name
 
 bart_name = args.bart_name
 opinion_first = args.opinion_first
+use_output_polarity = args.use_output_polarity
 
 def get_data():
     pipe = BartBPEABSAPipe(tokenizer=bart_name, opinion_first=opinion_first)
@@ -40,11 +42,8 @@ model = torch.load(model_path)
 model.to(device)
 pred = Predictor(model)
 
-offset = len(mapping) + 3 #わからん bos+eos+?
-mapping = list(mapping.keys())
-
-output = pred.predict(data_bundle.get_dataset('test'))
-print(data_bundle.get_dataset('test'))
+data = data_bundle.get_dataset('test')
+output = pred.predict(data)
 
 #data_bundle.get_dataset('train') class fastNLP.core.dataset.DataSet
 #pprint.pprint(output)
@@ -52,28 +51,11 @@ print(data_bundle.get_dataset('test'))
 # i.e. array([[ 0, 13, 13, 12, 12,  2, 15, 15, 12, 12,  2, 18, 18, 18, 12, 12, 2, 1]])
 output = output['pred']
 output = list(itertools.chain.from_iterable(output))
-aos = list()
-for line in output:
-    p_line = list()
-    line = line[1:-1]
-    if len(line) % 5 != 0:
-        p_line.append('error')
-    else:
-        for i in range(len(line)//5):
-            l = line[i*5:i*5+5].tolist()
-            sent = l[-1] - 2
-            l = l[0:-1]
-            l = list(map(lambda x: x-offset, l))
-            l[1] += 1
-            l[3] += 1
-            l.append(mapping[sent])
-            p_line.append(l)
-            
-    aos.append(p_line)
-target_span = list(data_bundle.get_dataset('test')['target_span'])
-tgt_s = list()
-for t in target_span:
-    s_line = [l[-1] for l in t] 
-    tgt_s.append(s_line)
 
-pprint.pprint(list(zip(aos,tgt_s)))
+if use_output_polarity:
+    aos = process_aos(output, mapping)
+else:
+    aos = process_aos(output, mapping, data)
+
+output_path = ('/home/drrndrrnprn/nlp/ABST/BARTABSA/output/' + dataset_name + '/output_' + os.path.basename(model_path) + '.json')
+output_json(data, aos, output_path)
--- BARTABSA/peng/model/utils.py
+++ BARTABSA/peng/model/utils.py
@@ -1,9 +1,10 @@
-import numpy as np
+import json
+import os
 
+import numpy as np
 
 def get_max_len_max_len_a(data_bundle, max_len=10):
     """
-
     :param data_bundle:
     :param max_len:
     :return:
@@ -26,3 +27,76 @@ def get_num_parameters(model):
     for name, param in model.named_parameters():
         num_param += np.prod(param.size())
     print(f"The number of parameters is {num_param}")
+    
+
+def process_aos(output, mapping, data=None):
+    aos = list()
+    offset = len(mapping) + 3
+    mapping = list(mapping.keys())
+    
+    for line in output:
+        p_line = list()
+        line = list(line)
+        line = line[1:-1]
+        if len(line) % 5 != 0:
+            p_line.append('error')
+        else:
+            for i in range(len(line)//5):
+                l = line[i*5:i*5+5]
+                sent = l[-1] - 2
+                l = l[0:-1]
+                l = list(map(lambda x: x-offset, l))
+                l[1] += 1
+                l[3] += 1
+                l.append(mapping[sent])
+                p_line.append(l)     
+        aos.append(p_line)
+        if data:
+            target_span = list(data['target_span'])
+            tgt_s = list()
+            new_aos = list()
+            for t in target_span:
+                s = [l[-1] for l in t] 
+            tgt_s.append(s)
+            for line, s in zip(aos, tgt_s):
+                line[-1] = s
+                new_aos.append(line)
+            aos = new_aos
+            
+    return aos
+
+
+def output_json(data, aos, output_path):
+    json_out = list()
+    for d_ins, aos_ins in zip(data, aos):# 極性のつけ方どうしよ
+        raw_words = d_ins['raw_words']
+        if aos_ins[0] == 'error':
+            continue
+        for i in range(len(aos_ins)):
+            if aos_ins[i][1] > len(raw_words) or aos_ins[i][3] > len(raw_words):
+                continue
+        aspects = list()
+        opinions = list()
+        for i in range(len(aos_ins)):
+            aspect =  \
+                {'index' : i,
+                'from' : int(aos_ins[i][0]),
+                'to' : int(aos_ins[i][1]),
+                'polarity' : aos_ins[i][4],
+                'term' : raw_words[aos_ins[i][0]:aos_ins[i][1]]}
+            opinion = \
+                {'index' : i,
+                'from' : int(aos_ins[i][2]),
+                'to' : int(aos_ins[i][3]),
+                'term' : raw_words[aos_ins[i][2]:aos_ins[i][3]]}
+            aspects.append(aspect)
+            opinions.append(opinion)
+        json_ins = \
+            {'raw_words': ' '.join(raw_words),
+            'words': raw_words,
+            'aspects': aspects,
+            'opinions': opinions}
+        json_out.append(json_ins)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, mode='w') as f:
+        json.dump(json_out, f, sort_keys=True, indent=4)
