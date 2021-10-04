d890f1575abfd5c33f5599fd4fbf421229cc94a8
drrndrrnprn <38527355+drrndrrnprn@users.noreply.github.com>
2021-10-04 14:40:25 +0900

    add: .py for preprocess data

---
+++ data/ana_preprocess.py
@@ -0,0 +1,13 @@
+#analyzed data(.json) --> raw text(.txt), aspect infomation(.txt)
+def extract_aspect_from_json():
+    pass
+#raw text(.txt) and aspect infomation(.txt) --> train(.txt), dev(.txt), test(.txt) 
+def separate_data():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
