7d049df1c2b0c0dde268ced2e36723333ade549b
drrndrrnprn <38527355+drrndrrnprn@users.noreply.github.com>
2021-11-01 13:31:38 +0900

    changed dir name

--- bartabst/models/__init__.py
+++ bartabst/models/__init__.py
@@ -3,5 +3,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .hub_interface import *  # noqa
-from .model import *  # noqa
+from bartabst.model.hub_interface import *  # noqa
+from bartabst.model.model import *  # noqa
